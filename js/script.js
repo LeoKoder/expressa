a7a904b9f884065f8b1ecde3011f360152fbf315
LeoKot <git@github.com:LeoKoder>
2021-11-01 16:01:03 +0300

    slider update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ window.addEventListener("DOMContentLoaded", function () {
   var myCarousel1 = document.querySelector("#new-m__slider");
   var carousel1 = new bootstrap.Carousel(myCarousel1);
 
-  var myCarouse2 = document.querySelector("#popular-m__slider");
+  var myCarousel2 = document.querySelector("#popular-m__slider");
   var carousel2 = new bootstrap.Carousel(myCarousel2);
 });
